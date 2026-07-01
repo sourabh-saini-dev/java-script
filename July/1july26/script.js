98d932e2c2f2b2d30bcd6f035713f00c2c0ba9aa
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-07-01 16:55:36 +0530

    updated

--- July/1july26/script.js
+++ July/1july26/script.js
@@ -1,169 +1,135 @@
-
-       
-
-    //    let str = " my name is sourabh saini i blong to kotputli jaipur rajasthan ajmer"
-    //     let ans = str.split(" ").map((val)=> val.charAt(0).toUpperCase()+val.slice(1)).join(" ")
-    //     console.log(ans)
-
-
+//    let str = " my name is sourabh saini i blong to kotputli jaipur rajasthan ajmer"
+//     let ans = str.split(" ").map((val)=> val.charAt(0).toUpperCase()+val.slice(1)).join(" ")
+//     console.log(ans)
 
 //const { log } = require("node:console")
 
 //const { json } = require("node:stream/consumers");
 
+//    let str = " abcdaaba"
+//     let res = str.split("").filter((ch)=> ch==="a").length
+//     console.log(res)
 
-        //    let str = " abcdaaba"
-        //     let res = str.split("").filter((ch)=> ch==="a").length
-        //     console.log(res)
+//    let str = " abcdaabartumrfdghj"
+//     let res = str.split("").filter((ch) => "a,e,i,o,u".includes(ch))
+//     console.log(res)
 
+// let user = {
+//      name: "henamt kumar sharma",
+//       age: 56,
+// }
 
-            
-        //    let str = " abcdaabartumrfdghj"
-        //     let res = str.split("").filter((ch) => "a,e,i,o,u".includes(ch))
-        //     console.log(res)
+// localStorage.setItem("name", JSON.parse())
+//   let ans = localStorage.getItem("name")
 
+//    console.log(ans)
+//     console.log(typeof ans)
 
-        // let user = {
-        //      name: "henamt kumar sharma",
-        //       age: 56,
-        // }
+//   async  function abc(){
+//         return "hello"
+//     }
+//      let a = abc()
+//      console.log(a);
+//      a.then(res=> console.log(res)).catch(err => console.log(err))
+// async yah normal function ko promise returining function me bnati hai
 
+// await   yah async me use kia jata hai promise resolve hone ka wait karna
 
-        // localStorage.setItem("name", JSON.parse())
-        //   let ans = localStorage.getItem("name")
-           
+//  function abc(){
+//    return   new Promise(resolve=> {
+//       resolve("resolved")
+//    })
+//  }
 
-        //    console.log(ans)
-        //     console.log(typeof ans)
+//  abc().then(res=> console.log(res))
 
-
-    //   async  function abc(){
-    //         return "hello"
-    //     }
-    //      let a = abc()
-    //      console.log(a);
-    //      a.then(res=> console.log(res)).catch(err => console.log(err))
-         // async yah normal function ko promise returining function me bnati hai 
-
-              
-
-          
-         // await   yah async me use kia jata hai promise resolve hone ka wait karna 
-
-          //  function abc(){
-          //    return   new Promise(resolve=> {
-          //       resolve("resolved")
-          //    })
-          //  }
-
-          //  abc().then(res=> console.log(res))
-
-
-
-
-
-
-
-    const students = [
+const students = [
   {
     id: 1,
     name: "Rahul",
     age: 22,
     city: "Jaipur",
-    marks: 85
+    marks: 85,
   },
   {
     id: 2,
     name: "Priya",
     age: 20,
     city: "Delhi",
-    marks: 92
+    marks: 92,
   },
   {
     id: 3,
     name: "Amit",
     age: 23,
     city: "Jaipur",
-    marks: 78
+    marks: 78,
   },
   {
     id: 4,
     name: "Neha",
     age: 21,
     city: "Mumbai",
-    marks: 88
+    marks: 88,
   },
   {
     id: 5,
     name: "Rohit",
     age: 24,
     city: "Delhi",
-    marks: 65
-  }
+    marks: 65,
+  },
 ];
 // Questions
 
 // Sabhi students ke name ka array banao.
 
-
 //  let ans = students.map((val) => val.name)
 //     console.log(ans);
-    
 
 // Sirf Jaipur ke students nikalo
 
-
-  //  let ans = students.filter((val)=> val.city==="Jaipur")
-  //    console.log(ans)
-
-
+//  let ans = students.filter((val)=> val.city==="Jaipur")
+//    console.log(ans)
 
 // Jinki marks 80 se jyada hain unhe nikalo.
 
-
 //  let ans = students.filter((val)=> val.marks > 80)
 //       console.log(ans);
-      
 
-
-// Sabhi students ke marks ka total nikalo. 
+// Sabhi students ke marks ka total nikalo.
 
 //  let ans = students.reduce((acc,val) =>{
 //   return acc +val.marks
 //  },0)
 //   console.log(ans);
-  
-
 
 // Average marks nikalo. (reduce)
-           
+
 //  let ans = students.reduce((acc, val)=> {
 //    return  acc+val.marks
 //  },0)
 //   let average = ans/students.length
 //      console.log(average);
-     
+
 // Sabhi students ke marks me 5 marks add karo.
 
-  // let ans = students.map((val) => val.marks +5)
-  //    console.log(ans);
-     
+// let ans = students.map((val) => val.marks +5)
+//    console.log(ans);
 
 // Rohit ka object find karo. (find)
-   
 
 //  let ans = students.find((val) => val.name === "Rohit")
 //  console.log(ans);
- 
-      
-// Check karo ki koi student Mumbai se hai ya nahi. 
-      // let ans = students.some((val) => val.city === "Mumbai")
-      //   console.log(ans);
-        
-// Check karo ki sabhi students ke marks 60 se upar hain ya nahi. 
-      //  let ans  = students.every((val)=> val.marks >60)
-      //    console.log(ans);
-         
+
+// Check karo ki koi student Mumbai se hai ya nahi.
+// let ans = students.some((val) => val.city === "Mumbai")
+//   console.log(ans);
+
+// Check karo ki sabhi students ke marks 60 se upar hain ya nahi.
+//  let ans  = students.every((val)=> val.marks >60)
+//    console.log(ans);
+
 // Sirf name aur city ka naya object banao.
 
 //  let ans = students.map((val) => {
@@ -171,8 +137,8 @@
 //     name: val.name,
 //     city: val.city,
 //   }
- 
+
 //  })
 //    console.log(ans);
-   
+
 // Example output:
