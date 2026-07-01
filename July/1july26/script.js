80d62c178de51275c56290e2701c9c5ee830b602
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-07-01 16:49:08 +0530

    1jule26

--- July/1july26/script.js
+++ July/1july26/script.js
@@ -5,7 +5,7 @@
     //     let ans = str.split(" ").map((val)=> val.charAt(0).toUpperCase()+val.slice(1)).join(" ")
     //     console.log(ans)
 
-const { log } = require("console")
+
 
 //const { log } = require("node:console")
 
@@ -50,10 +50,129 @@ const { log } = require("console")
           
          // await   yah async me use kia jata hai promise resolve hone ka wait karna 
 
-           function abc(){
-             return   new Promise(resolve=> {
-                resolve("resolved")
-             })
-           }
+          //  function abc(){
+          //    return   new Promise(resolve=> {
+          //       resolve("resolved")
+          //    })
+          //  }
 
-           abc().then(res=> console.log(res))
+          //  abc().then(res=> console.log(res))
+
+
+
+
+
+
+
+    const students = [
+  {
+    id: 1,
+    name: "Rahul",
+    age: 22,
+    city: "Jaipur",
+    marks: 85
+  },
+  {
+    id: 2,
+    name: "Priya",
+    age: 20,
+    city: "Delhi",
+    marks: 92
+  },
+  {
+    id: 3,
+    name: "Amit",
+    age: 23,
+    city: "Jaipur",
+    marks: 78
+  },
+  {
+    id: 4,
+    name: "Neha",
+    age: 21,
+    city: "Mumbai",
+    marks: 88
+  },
+  {
+    id: 5,
+    name: "Rohit",
+    age: 24,
+    city: "Delhi",
+    marks: 65
+  }
+];
+// Questions
+
+// Sabhi students ke name ka array banao.
+
+
+//  let ans = students.map((val) => val.name)
+//     console.log(ans);
+    
+
+// Sirf Jaipur ke students nikalo
+
+
+  //  let ans = students.filter((val)=> val.city==="Jaipur")
+  //    console.log(ans)
+
+
+
+// Jinki marks 80 se jyada hain unhe nikalo.
+
+
+//  let ans = students.filter((val)=> val.marks > 80)
+//       console.log(ans);
+      
+
+
+// Sabhi students ke marks ka total nikalo. 
+
+//  let ans = students.reduce((acc,val) =>{
+//   return acc +val.marks
+//  },0)
+//   console.log(ans);
+  
+
+
+// Average marks nikalo. (reduce)
+           
+//  let ans = students.reduce((acc, val)=> {
+//    return  acc+val.marks
+//  },0)
+//   let average = ans/students.length
+//      console.log(average);
+     
+// Sabhi students ke marks me 5 marks add karo.
+
+  // let ans = students.map((val) => val.marks +5)
+  //    console.log(ans);
+     
+
+// Rohit ka object find karo. (find)
+   
+
+//  let ans = students.find((val) => val.name === "Rohit")
+//  console.log(ans);
+ 
+      
+// Check karo ki koi student Mumbai se hai ya nahi. 
+      // let ans = students.some((val) => val.city === "Mumbai")
+      //   console.log(ans);
+        
+// Check karo ki sabhi students ke marks 60 se upar hain ya nahi. 
+      //  let ans  = students.every((val)=> val.marks >60)
+      //    console.log(ans);
+         
+// Sirf name aur city ka naya object banao.
+
+//  let ans = students.map((val) => {
+//   return {
+//     name: val.name,
+//     city: val.city,
+//   }
+ 
+//  })
+//    console.log(ans);
+   
+// Example output:
