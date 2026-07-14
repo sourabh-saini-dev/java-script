b380972d365806526c83b15309b4180d4e5bf45f
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-07-14 23:09:14 +0530

    15july26

---
+++ July/15July26/script.js
@@ -0,0 +1,10 @@
+ 
+     
+         let arr = [1,2,3,2,3,4,5,4]
+            for(let i = 0; i<arr.length; i++){
+                for(let j = i+1; j<arr.length; j++){
+                    if(arr[i]===arr[j]){
+                        console.log(arr[i])
+                    }
+                }
+            }
