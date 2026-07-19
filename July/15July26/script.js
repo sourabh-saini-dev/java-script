4e267abaa4da4a4630df9a36740afb2c2e05bf8d
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-07-20 01:15:11 +0530

    added commit

--- July/15July26/script.js
+++ July/15July26/script.js
@@ -1,6 +1,6 @@
        //duplicate element delete by nested array
 
-const { log } = require("node:console")
+
 
      
         //  let arr = [1,2,3,2,3,4,5,4]
@@ -13,9 +13,149 @@ const { log } = require("node:console")
         //     }
 
                   
-               let arr = [ 3,4,5,]
-                let arr1 = [ 3,4,6,7,8]
+            //    let arr = [ 3,4,5,]
+            //     let arr1 = [ 3,4,6,7,8]
 
-                 let ans = [...new Set([...arr,...arr1])]
-                  let rev = ans.reverse().join("")
-                   console.log(rev)
+            //      let ans = [...new Set([...arr,...arr1])]
+            //       let rev = ans.reverse().join("")
+            //        console.log(rev)
+                  
+
+
+
+// Reverse array (without reverse())
+//   let arr = [ 1,2,3,4]
+//         let rev = []
+//          for( let i = arr.length-1; i>=0; i--){
+//             rev.push(arr[i])
+             
+//          }
+//           console.log(rev)
+// Move zeroes to end
+    //   let arr = [ 1,0,5,0, 5,0,7,0]
+          
+    //   let res = [];
+
+    //      for(let i = 0; i<arr.length; i++){
+    //          if(arr[i] !==0){
+    //             res.push(arr[i])
+
+    //          }
+    //      }
+    //        for(let i = 0; i<arr.length; i++){
+    //              if(arr[i] === 0){
+    //                 res.push(arr[i])
+    //              }
+    //        }
+    //        console.log(res)
+
+// Find duplicate elements
+            //  let arr = [ 1,1,2,3,3,5,6,7,6]
+            //    for(let i = 0; i<arr.length; i++){
+            //      for(let j = i+1; j<arr.length; j++){
+            //          if(arr[i] === arr[j]){
+            //             console.log(arr[i])
+            //          }
+            //      }
+            //    }
+// Remove duplicates
+// let arr = [1, 1, 2, 3, 3, 5, 6, 6, 7];
+// let res = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] != arr[i + 1]) {
+//         res.push(arr[i]);
+//     }
+// }
+
+// console.log(res);
+// Find largest element
+//   let arr = ["html", "javascript"];
+// let longest = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i].length > longest.length) {
+//         longest = arr[i];
+//     }
+// }
+
+// console.log(longest);
+
+// Find second largest element
+      
+// Find smallest element
+// Find missing number (1 to N)
+//  let arr = [ 1,2,3,5]
+//   let n = 5;
+//   let sum = 0;
+//   for(let i = 0; i<arr.length; i++){
+//       sum+=arr[i]
+//   }
+//   let total = (n*(n+1)/2)
+//   console.log(total-sum)
+// Merge two arrays
+    
+// Count even and odd  numbers
+// Strings
+// Reverse string
+    //  let strr = "1234"
+    //   let ans = strr.split("").reverse().join("")
+    //      console.log(ans)
+// Check palindrome
+// Count vowels
+// Count words
+// Remove duplicate characters
+// Count frequency of each character
+// Find longest word
+// Capitalize first letter of each word
+// Check anagram
+// Remove spaces
+// Objects
+// Swap key and value
+// Count number of keys
+// Print all keys
+// Print all values
+// Merge two objects
+// DOM
+// Create element
+// Change text
+// Change CSS using JS
+// Add button click event
+// Input value display
+// Dark/Light mode toggle
+// Counter (+ / -)
+// Show/Hide password
+// Search filter
+// Todo list
+// Fetch API
+// Fetch users from API
+// Display data using map()
+// Search user by name
+// Filter users
+// Store data in localStorage
+// Practice
+// Bubble Sort
+// Linear Search
+// Binary Search
+// Fibonacci
+// Factorial
+// Prime Number
+// Armstrong Number
+// Sum of digits
+// Reverse number
+// Count digits
+    
+//   let arr = [ 1,3,3,4,5,61,7]
+//    let count = 0;
+//   for(let i = 0; i<arr.length; i++){
+//      for(let j = i+1; j<arr.length; j++){
+//         if(arr[i]===arr[j]){
+//             count++
+
+//         }
+//      }
+//      if(count===1){
+//         console.log(arr[i])
+
+//      }
+//   }
