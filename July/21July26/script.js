3fcf94e144daae277f68bf6b8e826bc8eb080ea5
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-07-22 12:33:46 +0530

    22july26

--- July/21July26/script.js
+++ July/21July26/script.js
@@ -1,3 +1,46 @@
   
 
+        // let arr = [12,32,45,65]
+        //  let ans= arr.filter((val)=> val%2===0)
+
        
+
+         
+        //  console.log(ans);
+
+
+
+
+
+
+        // let arr = [ 12,32,45,76]
+        //  let ans = []
+        //   for(let i = arr.length-1; i>=0; i--){
+        //     ans.push(arr[i])
+
+            
+        //   }
+        //   let res = ans.sort((a,b)=> a-b)
+
+        //    let sum = res.filter((val)=> val%2===0)
+        //     let rev = sum.reverse().join(" ")
+
+        //   console.log(rev);
+
+
+           
+        //    let str = "javascript, mondodb html css react nodejs tailwind"
+        //      let ans = str.split(" ").map((val)=> val.charAt().toUpperCase()+val.slice(1)).join(" ")
+        //        console.log(ans);
+               
+
+
+
+                // let str = "javascript, mondodb html css react nodejs tailwind";
+
+                // let res = str.split("").filter((ch)=> "a,e,i,o,u".includes(ch) ).length;
+                //   console.log(res)
+                
+
+            
+          
---
+++ July/22July26/script.js
@@ -0,0 +1,21 @@
+// const { log } = require("node:console");
+
+// let p = new Promise((resolve, reject) => {
+//     let a = 10;
+
+//     if (a) {
+//         resolve("resolved");
+//     } else {
+//         reject("rejected");
+//     }
+// });
+
+// p.then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
+// .finally(()=>{
+//      console.log("finally")
+// })
