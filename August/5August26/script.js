0d9ef10db4debc64094d31c9163fc39225d3415c
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-05 01:36:13 +0530

    added

--- August/5August26/script.js
+++ August/5August26/script.js
@@ -39,6 +39,31 @@
 //      rev.push(arr[i])
 //   }
 //   console.log(rev);
+
+
+
+// let arr = [32,43,43,43,56,76,56,87]
+// let rev=[]
+//   for(let i = arr.length-1 ; i>=0; i--){
+//      rev.push(arr[i])
+//   }
+
+  
+//   console.log(rev);
+
+
+// let arr = [32,43,43,43,56,76,56,87]
+//  let ans = arr.filter((val)=> val % 2 === 0)
+//     let an = ans.sort((a,b)=> a-b)
+//   console.log(ans);
+  
+
+
+
+// let arr = [32,43,43,43,56,76,56,87]
+//  let ans = arr.filter((val)=> val % 2 === 0)
+//     let an = ans.sort((a,b)=> b-a)
+//   console.log(ans);
   
  
   
