2f78f616a5ca4b8ee372f8c32bdb15187dfd401d
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-06 01:08:05 +0530

    added

--- August/6August/script.js
+++ August/6August/script.js
@@ -20,21 +20,42 @@
     //  })
     //   console.log(ans);
 
-      let arr = [2,2,3,4,2,5,6]
+    //   let arr = [2,2,3,4,2,5,6]
 
-       for(let i = 0; i<arr.length; i++){
-         let count = 0;
-          for( let j = 0; j<arr.length; j++){
-             if(arr[i] === arr[j]){
-                 count++
-             }
-          }
-          if(count==1){
-            console.log(arr[i]);
+    //    for(let i = 0; i<arr.length; i++){
+    //      let count = 0;
+    //       for( let j = 0; j<arr.length; j++){
+    //          if(arr[i] === arr[j]){
+    //              count++
+    //          }
+    //       }
+    //       if(count==1){
+    //         console.log(arr[i]);
             
-          }
-       }
+    //       }
+    //    }
       
+
+       let arr = [4,5,0,8,0,8,0,6]
+       let res = []
+        for(let i = 0; i<arr.length; i++){
+             if(arr[i]!==0){
+                res.push(arr[i])
+
+             }
+        
+             }
+             for(let j = 0; j<arr.length; j++){
+                 if(arr[j]===0){
+                    res.push(arr[j])
+
+                 }
+             }
+              console.log(res);
+              
+        
+      
+        
 
 
       
