b00ceb17e7f8361c42a5a9e73234aeedfacf517a
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-09 00:02:52 +0530

    added

--- August/8August26/script.js
+++ August/8August26/script.js
@@ -25,7 +25,26 @@
           
         //   let str = 'sourabh saini'
         //     console.log( str.replace("sourabh", "kotputli" ));
-        
+
             //  let arr = [ "s" , "g"]
             //   console.log(arr.join(""));
               
+
+
+              let arr = [ 1,2, 0,3,0,4,3,2]
+              let res = []
+
+               for(let i = 0; i<arr.length;i++){
+                if(arr[i] !== 0){
+                    res.push(arr[i])
+                }
+                    }
+                    for(let j = 0; j<arr.length; j++){
+                if(arr[j] === 0){
+                 res.push(arr[j])
+            
+                   }
+               }
+                console.log(res);
+                
+               
