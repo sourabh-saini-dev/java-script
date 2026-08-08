5d919733ebb9716cf5c4190cec829363e5f8003b
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-08 23:55:08 +0530

    8August26 added

---
+++ August/8August26/script.js
@@ -0,0 +1,31 @@
+    
+
+      
+       
+        
+         
+        //   let str = ["mango" , "programing", "javascript.js"]
+        //   let ans = 0
+        //    for(let i = str.length-1; i>=0; i--){
+        //         ans =  ans +str[i]
+
+
+        //    }
+        //       console.log(ans)
+
+
+
+
+         
+        //   let str =  " mango sourabh saini jaipur rajashthan ajmer";
+
+        //   let ans = str.split("").filter((ch)=> "aeiou".includes(ch)).length
+        //      console.log(ans);
+
+          
+        //   let str = 'sourabh saini'
+        //     console.log( str.replace("sourabh", "kotputli" ));
+        
+            //  let arr = [ "s" , "g"]
+            //   console.log(arr.join(""));
+              
