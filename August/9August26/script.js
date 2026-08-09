e2823175f492b2864e4c502370eed7ba50b501e4
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-09 14:28:51 +0530

    9August26

---
+++ August/9August26/script.js
@@ -0,0 +1,31 @@
+   
+   
+    // for(let i = 0; i<51; i++){
+
+const { log } = require("node:console")
+
+
+
+         
+    //     if(i%3===0 && i%5 === 0){
+            
+    //       console.log(i)
+    //     }
+    
+    // }
+      
+        // for(let i = 1; i<11; i++){
+        //     console.log(`5 * ${ i}  = ${5 * i}`)
+        // }
+
+        // ye manuvally kaam nhe karta multiply items ke liye ye signle ke liye kaam karta hai hmesha//
+
+      
+
+
+        // let str = "mango"
+        //   console.log("hello" , "mango")
+
+
+    //  let str = 'apple'
+    //  console.log(`hello${str}`)
