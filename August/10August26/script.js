5841a1301b0ff33786abdf0aee56e71e06fd29a2
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-10 02:35:25 +0530

    10August26

---
+++ August/10August26/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+    //    function abc(...a){
+    //     console.log(a)
+
+    //    }
+    //    abc(12,3,4,5,6,7)
+
+
+
+    //   let arr = [ 1,2,3,4,5]
+    //    let arr1 = [ 5,4,6,7,8,9]
+    //    let arr2 = [...arr,...arr1 ,70,80]
+       
+    //     console.log(arr2);
+
+
+
+
+        
+    //   let arr = [ 1,2,3,4,5]
+    //    let arr1 = [ 5,4,6,7,8,9]
+    //    let arr2 = [...arr,...arr1 ,70,80]
+
+    //    let sum  = arr2.reduce((acc,val)=> acc+val)
+
+       
+    //     console.log(sum);
+        
+        
+     
+        
