ff6dc5c09bc46392bf1aa155c35cd62c5a99f201
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-10 22:54:51 +0530

    added

--- August/10August26/script.js
+++ August/10August26/script.js
@@ -32,4 +32,82 @@
         
         
      
-        
+      
+
+
+    //    let a = 10;
+    //    console.log( typeof a);  // type of check
+
+
+       
+    //    let a = 10;
+    //    console.log( typeof a.toString());  // convert in string
+       
+       
+
+            //   let a = 13.98667
+            //   console.log( a.toFixed(12));  // decimal number fixed karna 
+              
+
+
+            //  let a = 54.32
+            //  console.log(a.toPrecision(34));  number or decimal number ke length fixed karta hai 
+
+
+
+                //  console.log(parseInt("12.76"));
+                //      console.log(parseInt("-12.76"));
+                        
+                //      console.log(parseInt("12/abc"));
+                            
+                       
+                //      console.log(parseInt("abc/76"));
+
+
+
+
+                // console.log(parseFloat("12.32"));
+                //  console.log(parseFloat("-12.32"));
+                //   console.log(parseFloat("abc/32"));
+                //    console.log(parseFloat("12/32"));
+                //     console.log(parseFloat("12/asd"));
+                
+                 
+             
+                // let arr = [ 12,3,46]
+                // let ans = arr.push(6)
+                // console.log(ans)
+
+            
+                //  let arr = [1,2,3,4]
+                //  let ans = arr.unshift(21)
+                //  console.log(ans);
+
+
+
+                //   let arr = [ 12,43,56,78,90]
+                //    let res = arr.find((val)=> val>10)
+                //       console.log(res);  
+
+
+                // let arr = [ 1,23,45,65,67]
+                //  let ans = arr.filter((val)=> val>10)
+                //  console.log(ans);
+
+                    // let arr = ["myname is sourabh saini"]
+                    //  let ans = arr.map((val)=> val+"@gmail.com123")
+                    //   console.log(ans);
+
+
+
+                    //   let arr = [ 1,2,3,4,5]
+                    //    let ans = arr.reverse()
+                    //    let sum = ans.reduce((acc,val)=> acc+val,0)
+                        
+                    //       console.log(sum);
+                          
+                      
+                 
+                      
+                 
+                 
