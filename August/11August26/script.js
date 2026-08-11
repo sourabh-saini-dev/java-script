5937654ddc3b29fa1776b0de1aeeaa7118a58f06
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-11 23:48:56 +0530

    added

---
+++ August/11August26/script.js
@@ -0,0 +1,33 @@
+
+// let obj = {
+//     name: "sourabh",
+//     age: 25,
+//     address: "kotputli",
+    
+//     city:{
+
+//         role: "ahmedabad",
+//         country:"australia",
+
+//     }
+// }
+
+// //    console.log(obj);
+
+//         // console.log(obj.name);
+//         //   console.log(obj.age);
+//         //     console.log(obj.city.coutntry);
+
+//         // console.log(obj["name"] );  bracket notion
+
+
+//          let obj1 = structuredClone(obj)
+//             obj.city.country = "jaipur",
+//              console.log(obj);
+//              console.log(obj1);
+             
+                    
+         
+          
+        
+   
