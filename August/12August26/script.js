0592bd0b6ce74a2637cb0b264115c92c3c3f7c2e
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-12 14:52:09 +0530

    added

--- August/12August26/script.js
+++ August/12August26/script.js
@@ -28,7 +28,66 @@
 
            
         //     console.log(SumDigit(12345));
-            
+
+
+
+                // let a = 1020304050
+                 
+                // let last;
+                // let count = 0;
+                // while(a){
+                //     last = a%10;
+                //       if(last === 0){
+                //         count++
+
+                //       }
+                //       a = Math.floor(a /10)
+                // }
+                //   console.log(count);
+                   
+                  
+                 
+                  
+                 
+                    //  function SumDigit(n){
+                    //      let sum = 0;
+                    //      let last;
+                    //      while(n){
+                    //         last = n%10;
+                    //         sum = sum+last
+                    //         n = Math.floor(n/10)
+                    //      }
+                    //      return sum
+                    //  }
+
+                    //  let n = Number(prompt("enter the value of number"))
+                    //  console.log( "sum =",SumDigit(n));
+
+
+
+                    function fetchData(){
+                        return new Promise((resolve)=>{
+                            resolve("data")
+                        })
+                    }
+                    fetchData().then((res)=>{
+                         console.log(res);
+                         
+                    })
+
+
+                    async function abc(){
+                        console.log("start")
+                        let result = await fetchData()
+                        console.log(result)
+                        console.log("end")
+
+                    }
+                    abc()
+
+                     
+
+                  
 
             
            
