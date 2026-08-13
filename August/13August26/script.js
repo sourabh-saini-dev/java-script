6d5b0623d75119d115e71b98da3d82964b5a3616
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-08-13 23:54:32 +0530

    13August26

--- August/13August26/script.js
+++ August/13August26/script.js
@@ -1,53 +1,4 @@
-     
 
-       
-        
-        //  let n = 3456;
-        //   let sum = 0
-        //   let last;
-        //   while(n){
-        //     last = n%10;
-        //      sum = sum+last;
-        //      n = Math.floor(n/10)
-        //   }
-        //    console.log(sum);
-
-           
-
-        //   function SumDigit(n){
-        //        let sum = 0;
-        //        let last;
-
-        //        while(n){
-        //           last = n%10;
-        //           sum = sum+last
-        //           n = Math.floor(n /10)
-        //        }
-        //        return sum
-        //   }
-
-           
-        //     console.log(SumDigit(12345));
-
-
-
-                // let a = 1020304050
-                 
-                // let last;
-                // let count = 0;
-                // while(a){
-                //     last = a%10;
-                //       if(last === 0){
-                //         count++
-
-                //       }
-                //       a = Math.floor(a /10)
-                // }
-                //   console.log(count);
-                   
-                  
-                 
-                  
                  
                     //  function SumDigit(n){
                     //      let sum = 0;
@@ -107,8 +58,3 @@
                     //   }
                     //    console.log(res);
                      
-
-                  
-
-            
-           
